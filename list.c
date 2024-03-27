52879ad4b5b1afc4fc55098cfaab00d6627f8419
Your Name <you@example.com>
2024-03-27 18:37:50 +0000

    new try

--- list.c
+++ list.c
@@ -37,9 +37,8 @@ List * createList() {
 }
 /*La primera retorna el dato del primer nodo de la lista (`head`) y actualiza el current para que apunte a ese nodo. */
 void * firstList(List * list) {
-  if (list->head != NULL) {
-    list->current = list->head;
-    return list->head->data;
+  if (list->head == NULL) {
+    return NULL;
   }
 }
 
